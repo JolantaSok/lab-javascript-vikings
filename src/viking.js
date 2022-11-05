af0d187b2c11035f1e1d786eb3354da85305b1e7
JolantaSok <jolanta.vingre@inbox.lv>
2022-11-05 09:57:09 +0100

    Empty

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,15 @@
 // Soldier
-class Soldier {}lllllll
+class Soldier {
+    constructor(health, strength) {
+        this.health = soldiersHealth;
+        this.strength = soldiersStrength;
+    }
+    attack () {
+        return this.strength()
+}
+}
+let soldier1 = new Soldier (this.soldiersHealth)
+
 
 // Viking
 class Viking {}
